2d01d951a183e577c3a60f56d99cb29726723142
JonatanRoldan <jroldan42@gmail.com>
2021-11-08 20:45:52 -0800

    make it better

--- src/routes/carro.routes.js
+++ src/routes/carro.routes.js
@@ -4,6 +4,7 @@ const router = Router();
 const carroCtrl = require("../controllers/carro.controller");
 
 router.get("/", carroCtrl.getcarros);
+
 router.post("/", carroCtrl.createcarro);
 router.get("/:id", carroCtrl.getcarro);
 router.put("/:id", carroCtrl.editcarro);
